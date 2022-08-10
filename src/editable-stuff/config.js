ba9695e4b8217137f0a678d05b2494380a1e65f9
roberio <roberio-almeida@hotmail.com>
2022-08-10 18:40:15 -0300

    chore: change photo extension

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -39,7 +39,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/perfil.jpg"),
+  imageLink: require("../editable-stuff/perfil.png"),
   imageSize: 375,
   message:
     "My name is Robério Dutra. With every passing minute, I learn more, in a process of constant study, improving hard and soft skills. That's my day-to-day, and I intend it to be that way forever. I'm an actor, I changed my career to be a Full-Stack developer with the intention of impacting people's lives with technology, which I have always been and always will be passionate about.",
@@ -67,12 +67,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/perfil.jpg"), 
+      img: require("../editable-stuff/perfil.png"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/perfil.jpg"), 
+      img: require("../editable-stuff/perfil.png"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
